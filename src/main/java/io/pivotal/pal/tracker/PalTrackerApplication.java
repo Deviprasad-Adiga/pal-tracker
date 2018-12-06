cce725d7546e770cca88d34d0d24c5f8506e27a3
Deviprasad Adiga <deviprasad40@gmail.com>
2018-12-06 22:16:32 +0530

    cloudNativeDeveloperDatabaseAccess

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,21 +4,33 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
 
+	private String driverClassName = "com.mysql.jdbc.Driver";
+	@Value("${SPRING_DATASOURCE_URL}")
+    private String url;
+    private String dbUsername = "tracker";
+    private String dbPassword = "";
+    
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
     @Bean
-    TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    JdbcTimeEntryRepository jdbcTimeEntryRepository() {
+    	DataSource dataSource = getDataSource();
+        return new JdbcTimeEntryRepository(dataSource);
     }
 
     @Bean
@@ -28,5 +40,14 @@ public class PalTrackerApplication {
                 .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) //ISODate
                 .modules(new JavaTimeModule())
                 .build();
+    }
+    
+    private DataSource getDataSource() {
+    	DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    	dataSource.setDriverClassName(driverClassName);
+    	dataSource.setUrl(url);
+    	dataSource.setUsername(dbUsername);
+    	dataSource.setPassword(dbPassword);
+    	return dataSource;
     }
 }
