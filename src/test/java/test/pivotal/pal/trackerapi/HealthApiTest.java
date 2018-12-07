106a431cc80a9dd3b090b35dc406136df8f7a460
Deviprasad Adiga <deviprasad40@gmail.com>
2018-12-07 19:23:57 +0530

    cloudNativeDeveloperBasicAuth

--- src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
+++ src/test/java/test/pivotal/pal/trackerapi/HealthApiTest.java
@@ -2,11 +2,15 @@ package test.pivotal.pal.trackerapi;
 
 import com.jayway.jsonpath.DocumentContext;
 import io.pivotal.pal.tracker.PalTrackerApplication;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,20 +23,28 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @SpringBootTest(classes = PalTrackerApplication.class, webEnvironment = RANDOM_PORT)
 public class HealthApiTest {
 
-    @Autowired
-    private TestRestTemplate restTemplate;
+	@LocalServerPort
+	private String port;
+	private TestRestTemplate restTemplate;
 
-    @Test
-    public void healthTest() {
-        ResponseEntity<String> response = this.restTemplate.getForEntity("/health", String.class);
+	@Before
+	public void setUp() throws Exception {
+		RestTemplateBuilder builder = new RestTemplateBuilder().rootUri("http://localhost:" + port)
+				.basicAuthorization("user", "password");
 
+		restTemplate = new TestRestTemplate(builder);
+	}
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	@Test
+	public void healthTest() {
+		ResponseEntity<String> response = this.restTemplate.getForEntity("/health", String.class);
 
-        DocumentContext healthJson = parse(response.getBody());
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-        assertThat(healthJson.read("$.status", String.class)).isEqualTo("UP");
-        assertThat(healthJson.read("$.db.status", String.class)).isEqualTo("UP");
-        assertThat(healthJson.read("$.diskSpace.status", String.class)).isEqualTo("UP");
-    }
+		DocumentContext healthJson = parse(response.getBody());
+
+		assertThat(healthJson.read("$.status", String.class)).isEqualTo("UP");
+		assertThat(healthJson.read("$.db.status", String.class)).isEqualTo("UP");
+		assertThat(healthJson.read("$.diskSpace.status", String.class)).isEqualTo("UP");
+	}
 }
